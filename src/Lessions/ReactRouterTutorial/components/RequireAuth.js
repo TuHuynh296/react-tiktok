80d02a2bdc2667d17d798b204b50ed04791b2733
Tu Huynh <tuhv@vnpt.vn>
2023-01-01 00:05:01 +0700

    handle call api to check is token valid

--- src/Lessions/ReactRouterTutorial/components/RequireAuth.js
+++ src/Lessions/ReactRouterTutorial/components/RequireAuth.js
@@ -1,24 +1,58 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "./auth";
+import { useEffect, useState } from "react";
 
 function RequireAuth({ children }) {
     const auth = useAuth();
     const location = useLocation();
+    const [isChecking, setIsChecking] = useState(true);
+    const [isAuthenticated , setIsAuthenticated ] = useState(false);
+    const navigate = useNavigate();
 
-    if (!auth.user) {
-        return (
-            <Navigate
-                to="/login"
-                state={{ path: location.pathname }}
-            />
-        )
-    }
+    useEffect(() => {
+        const checkIsAuthValid = async () => {
+            const getAuth = () => {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        resolve(true);
+                        //
+                        // reject(false);
+                    }, 2000);
+                });
+    
+            }
+
+            getAuth().then((value) => {
+                setIsAuthenticated(value);
+            }).catch((err) => {
+                setIsAuthenticated(false);
+            }).finally(() => {
+                setIsChecking(false);
+            })
+        }
+
+        checkIsAuthValid();
+    }, []);
+
 
     return (
         <>
-            {children}
+            {
+                isChecking
+                    ? <div>Loading ...</div>
+                    : isAuthenticated 
+                        ? children
+                        : (
+                            <Navigate
+                                to="/login"
+                                state={{ path: location.pathname }}
+                            />
+                        )
+            }
         </>
-    )
+    );
+
+    // https://stackoverflow.com/questions/70658559/protected-routes-in-react-router-v6-using-usestate-but-always-returns-false
 }
 
 export default RequireAuth;
--- src/index.js
+++ src/index.js
@@ -22,13 +22,13 @@ import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <TodoProvider> */}
+  // <React.StrictMode>
+    
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    {/* </TodoProvider> */}
-  </React.StrictMode>
+    
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
